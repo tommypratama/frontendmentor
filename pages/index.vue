59db8e0a4307955112d8947c8da84f514c235ada
Tommy Pratama <tommyhpratama@gmail.com>
2020-09-14 23:19:35 +0700

    Change mode to universal

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="bg-gradient-to-tr from-green-300 to to-purple-300 min-h-screen flex justify-center items-center">
-    <h1 class="text-4xl bg-gradient-to-r bg-clip-text text-transparent from-purple-700 to-green-600 font-bold text-center">Nuxtjs + Tailwindcss</h1>
+    <h1 class="text-4xl bg-gradient-to-r bg-clip-text text-transparent from-purple-700 to-green-600 font-bold text-center">Frontendmentor Challenge</h1>
   </div>
 </template>
